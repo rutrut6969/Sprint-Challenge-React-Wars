49e1d58b9cfedd6df00540d727566300a04d4ecb
rutrut6969 <rutledgeisaac6969@gmail.com>
2020-03-20 17:04:39 -0500

    Yup

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -8,7 +8,7 @@ import axios from 'axios';
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [name, setName] = useState([]);
+  const [names, setNames] = useState([]);
   const [data, setData] = useState([]);
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
@@ -19,22 +19,19 @@ const App = () => {
       .then(response => {
         console.log(response);
         // console.log(response.data.results.name);
-        const names = response.data.results;
-        names.forEach(e => {
-          // console.log(e.name);
-          name.push(e.name);
-        });
+        setNames(response.data.results);
+        console.log(response.data.results.starships);
         // console.log(data);
       })
       .catch(err => console.log(err));
   }, []);
-  console.log(name);
+
   return (
     <AppStyle className='App'>
       <h1 className='Header'>React Wars</h1>
-      <Wrapper>
+      <Wrapper className='wrapper'>
         {/* Contained Components in here: */}
-        <Cards name={name} />
+        <Cards names={names}></Cards>
       </Wrapper>
     </AppStyle>
   );
--- starwars/src/components/styledComponents.js
+++ starwars/src/components/styledComponents.js
@@ -2,24 +2,57 @@ import React from 'react';
 import styled from 'styled-components';
 
 export const Wrapper = styled.section`
-    // background-color: rgba(211, 211, 211, 0.7);
-    height: 30vh;
-    width 85%;
-    display flex;
-    justify-content: space-evenly;
+  // background-color: rgba(211, 211, 211, 0.7);
+  height: 30vh;
+  width: 90%;
+  height: 100vh;
+  display: flex;
+  justify-content: space-evenly;
+  flex-flow: row wrap;
 `;
 
 export const AppStyle = styled.div`
-  // background-image url(./sw-bg.jpg);
+  /* background-image: url('./sw-bg.jpg'); */
   display: flex;
   align-items: center;
   flex-direction: column;
+  width: 100%;
+  height: 100vh;
   //   z-index: -1;
 `;
 
-export const Card = styled.section`
-  background-color: rgba(211, 211, 211, 1);
-  height: 100px;
-  width: 100px;
+export const Card = styled.ul`
+  background-color: rgba(211, 211, 211, 0.75);
+  height: 200px;
+  width: 30%;
   border-radius: 10px;
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: flex-start;
+  align-items: flex-start;
+  box-shadow: 2px 2px 5px rgba(210, 188, 167, 0.9);
+`;
+
+export const Title = styled.h2`
+  color: black;
+  font-size: 1rem;
+  padding-top: 2%;
+  width: 95%;
+  padding-bottom: 2%;
+  border-bottom: 1px solid darkgray;
+`;
+export const CardAttrs = styled.li`
+  color: black;
+  margin-left: 2%;
+`;
+export const CardBody = styled.p`
+  color: red;
+`;
+
+export const CardWrap = styled.div`
+  width: 95%;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-evenly;
+  align-items: center;
 `;
